f7a20f138811fe2ead49cc8488e7ff32a7b19a40
shubunchu <shubunchu@gmail.com>
2024-11-14 00:01:56 -0500

    add POST function to players endpoint

--- frontend/app/api/players/route.js
+++ frontend/app/api/players/route.js
@@ -1,10 +1,21 @@
-import { NextResponse } from "next/server";
-import postgres from "postgres";
+import { NextResponse } from 'next/server';
+import postgres from 'postgres';
 
-const sql = postgres("postgresql://piko:pikopw@localhost:5432/piko-db?ssl=false");
+const sql = postgres('postgresql://piko:pikopw@localhost:5432/piko-db?ssl=false', {
+  idle_timeout: 20,
+  max_lifetime: 60 * 30
+});
 
 export async function GET() {
   return NextResponse.json(await sql`
     SELECT * FROM players;
+  `);
+}
+
+export async function POST(request) {
+  const { pname } = await request.json();
+  return NextResponse.json(await sql`
+    INSERT INTO players (pname)
+    VALUES (${pname});
   `);
 }
