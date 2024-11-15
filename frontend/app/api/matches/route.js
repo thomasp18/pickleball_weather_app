e1d69f6f144a03f3d2af398d90a9950f06c61d30
thomasp18 <twphomsouvanh@gmail.com>
2024-11-14 23:42:43 -0500

    Added lifetime for sql res from the matches api

--- frontend/app/api/matches/route.js
+++ frontend/app/api/matches/route.js
@@ -1,7 +1,10 @@
 import { NextResponse } from 'next/server';
 import postgres from 'postgres';
 
-const sql = postgres('postgresql://piko:pikopw@localhost:5432/piko-db?ssl=false');
+const sql = postgres('postgresql://piko:pikopw@localhost:5432/piko-db?ssl=false', {
+    idle_timeout: 20,
+    max_lifetime: 60 * 30
+});
 
 export async function GET() {
     return NextResponse.json(await sql`
