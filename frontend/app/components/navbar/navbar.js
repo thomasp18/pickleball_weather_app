729130986ebcb4e147d2be78c027632d1205cd1d
thomasp18 <twphomsouvanh@gmail.com>
2024-11-20 23:05:03 -0500

    navbar hamburger menu

--- frontend/app/components/navbar/navbar.js
+++ frontend/app/components/navbar/navbar.js
@@ -1,18 +1,20 @@
 'use client';
-import { Image, Nav } from 'react-bootstrap';
+import { Image, Nav, NavbarToggle } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import { NavDropdown } from 'react-bootstrap';
 
 function Navibar() {
     let currentPage = window.location.pathname;
     return (
-        <Navbar style={{ overflow: 'auto' }}>
+        <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">
                     <Image alt='' src='./favicon.ico' style={{ borderRadius: 7 }}></Image>
                 </Navbar.Brand>
-                <Navbar.Collapse>
-                    <Nav variant="underline">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto" variant='underline'>
                         <Nav.Item>
                             <Nav.Link href="/" className={`${currentPage === '/' ? 'active' : ''}`}>Home</Nav.Link>
                         </Nav.Item>
