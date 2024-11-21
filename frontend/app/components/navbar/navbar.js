4d6c920b751e8189001e003bd85725db453c6b12
thomasp18 <twphomsouvanh@gmail.com>
2024-11-20 23:27:28 -0500

    removed nav item tags

--- frontend/app/components/navbar/navbar.js
+++ frontend/app/components/navbar/navbar.js
@@ -13,18 +13,10 @@ function Navibar() {
                 <NavbarToggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" variant='underline' activeKey={window.location.pathname}>
-                        <Nav.Item>
-                            <Nav.Link href="/">Home</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href='/score' eventKey="/score">Scorekeeper</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href='/matches' eventKey="/matches">Matches</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href='/players' eventKey="/players">Players</Nav.Link>
-                        </Nav.Item>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href='/score' eventKey="/score">Scorekeeper</Nav.Link>
+                        <Nav.Link href='/matches' eventKey="/matches">Matches</Nav.Link>
+                        <Nav.Link href='/players' eventKey="/players">Players</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
