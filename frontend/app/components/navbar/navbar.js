6c7a784030be9456f4fe572b1416ccf295039e25
thomasp18 <twphomsouvanh@gmail.com>
2024-11-20 23:15:48 -0500

    removed className for each nav item

--- frontend/app/components/navbar/navbar.js
+++ frontend/app/components/navbar/navbar.js
@@ -13,18 +13,18 @@ function Navibar() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" variant='underline'>
+                    <Nav className="me-auto" variant='underline' activeKey={currentPage}>
                         <Nav.Item>
-                            <Nav.Link href="/" className={`${currentPage === '/' ? 'active' : ''}`}>Home</Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href='/score' eventKey="score" className={`${currentPage === '/score' ? 'active' : ''}`}>Scorekeeper</Nav.Link>
+                            <Nav.Link href='/score' eventKey="/score">Scorekeeper</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href='/matches' eventKey="matches" className={`${currentPage === '/matches' ? 'active' : ''}`} >Matches</Nav.Link>
+                            <Nav.Link href='/matches' eventKey="/matches">Matches</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href='/players' eventKey="players" className={`${currentPage === '/players' ? 'active' : ''}`}>Players</Nav.Link>
+                            <Nav.Link href='/players' eventKey="/players">Players</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
