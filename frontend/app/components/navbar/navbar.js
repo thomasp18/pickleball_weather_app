25e6cfdb1f35b8c097f782e99f13731f42d1b6ed
thomasp18 <twphomsouvanh@gmail.com>
2024-11-20 23:22:53 -0500

    removed currentPage variable

--- frontend/app/components/navbar/navbar.js
+++ frontend/app/components/navbar/navbar.js
@@ -4,16 +4,15 @@ import { Navbar } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
 function Navibar() {
-    let currentPage = window.location.pathname;
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">
                     <Image alt='' src='./favicon.ico' style={{ borderRadius: 7 }}></Image>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <NavbarToggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" variant='underline' activeKey={currentPage}>
+                    <Nav className="me-auto" variant='underline' activeKey={window.location.pathname}>
                         <Nav.Item>
                             <Nav.Link href="/">Home</Nav.Link>
                         </Nav.Item>
