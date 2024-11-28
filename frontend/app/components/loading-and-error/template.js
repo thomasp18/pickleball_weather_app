e40e82bad5d7495dc22fad0213ce91ca7a32c164
KyungjinJPark <kjparkcontact@gmail.com>
2024-11-27 23:11:45 -0500

    22 replaces loading and error emojis w svgs

--- frontend/app/components/loading-and-error/template.js
+++ frontend/app/components/loading-and-error/template.js
@@ -3,7 +3,7 @@ import styles from './template.module.css';
 function Template({ emoji, text, rotate }) {
   return <div className={styles.expand}>
     <div className={styles.emojiContainer}>
-      <p className={`${styles.emoji} ${rotate && styles.rotate}`}>{emoji}</p>
+      <div className={`${styles.emoji} ${rotate && styles.rotate}`}>{emoji}</div>
       <h1 className='display-3'>{text}</h1>
     </div>
   </div>;
