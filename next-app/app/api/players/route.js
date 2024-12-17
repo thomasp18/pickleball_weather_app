8a1871d581848f3daeafce144390bd4888f37e3e
shubunchu <shubunchu@gmail.com>
2024-12-17 00:55:03 -0500

    add input validation for `POST` function in players endpoint

--- next-app/app/api/players/route.js
+++ next-app/app/api/players/route.js
@@ -16,6 +16,29 @@ export async function GET() {
 
 export async function POST(request) {
   const { pname } = await request.json();
+
+  if (!pname || typeof pname !== 'string') {
+    return NextResponse.json(
+      { error: `${pname} must be a non-empty string value` },
+      { status: 400 },
+    );
+  }
+
+  const regex = /^[a-zA-Z]+$/;
+  if (!regex.test(pname)) {
+    return NextResponse.json(
+      { error: `${pname} can only include lowercase and uppercase letters` },
+      { status: 400 },
+    );
+  }
+
+  const query = await sql`
+    SELECT LOWER(pname) FROM players
+    WHERE LOWER(pname) = ${pname.toLowerCase()}`;
+  if (query.length !== 0) {
+    return NextResponse.json({ error: `${pname} already exists` }, { status: 400 });
+  }
+
   return NextResponse.json(
     await sql`
     INSERT INTO players (pname)
